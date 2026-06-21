e3cadda5c0f940b23848d279b1e57af6ab91cd15
YOUNGMIN <1201brian@gmail.com>
2026-06-21 16:56:42 +0900

    fix: 가천대 구형 SSL 핸드셰이크 오류 해결

--- scrapers/utils.py
+++ scrapers/utils.py
@@ -1,5 +1,8 @@
+import ssl
 import urllib3
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.ssl_ import create_urllib3_context
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -8,5 +11,17 @@ HEADERS = {
 }
 
 
+class LegacySSLAdapter(HTTPAdapter):
+    """구형 SSL 암호화를 사용하는 서버(gachon.ac.kr)를 위한 어댑터."""
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = create_urllib3_context()
+        ctx.set_ciphers("DEFAULT@SECLEVEL=1")
+        ctx.options |= ssl.OP_LEGACY_SERVER_CONNECT
+        kwargs["ssl_context"] = ctx
+        super().init_poolmanager(*args, **kwargs)
+
+
 def get(url, params=None):
-    return requests.get(url, params=params, headers=HEADERS, timeout=15, verify=False)
+    session = requests.Session()
+    session.mount("https://", LegacySSLAdapter())
+    return session.get(url, params=params, headers=HEADERS, timeout=15, verify=False)
